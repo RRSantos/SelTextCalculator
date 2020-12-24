5eb68ae05d0690872b52c07e8bf6e8fd38808b93
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2020-12-24 19:25:42 -0300

    initial commit

---
+++ src/SelTextCalculator/Main.cs
@@ -0,0 +1,77 @@
+﻿using SelTextCalculator;
+using System.Windows.Forms;
+using Kbg.NppPluginNET.PluginInfrastructure;
+using System;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "SelText Calculator";
+        internal const string SumNumbersMenuItemName = "Sum numbers in selection";
+        internal const string MultiplyNumbersMenuItemName = "Multiply numbers in selection";
+
+        static IScintillaGateway editor = new ScintillaGateway(PluginBase.GetCurrentScintilla());
+        static INotepadPPGateway notepad = new NotepadPPGateway();
+
+        public static void OnNotification(ScNotification notification)
+        {
+            // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
+        }
+
+        internal static void CommandMenuInit()
+        {
+            PluginBase.SetCommand(0, SumNumbersMenuItemName, sumNumbersInSelection, new ShortcutKey(true, true, false, Keys.A));
+            PluginBase.SetCommand(1, MultiplyNumbersMenuItemName, multiplyNumbersInSelection, new ShortcutKey(true, true, false, Keys.M));
+        }
+
+        internal static void SetToolBarIcon()
+        {
+
+        }
+
+        internal static void PluginCleanUp()
+        {
+
+        }
+
+
+        internal static void sumNumbersInSelection()
+        {
+            doOperation(StringCalculator.Sum, SumNumbersMenuItemName);
+            //string selectedText = editor.GetSelText();
+            //decimal result = StringCalculator.Sum(selectedText);            
+            //MessageBox.Show(result.ToString(), SumNumbersMenuItemName);
+        }
+
+        internal static void multiplyNumbersInSelection()
+        {
+            doOperation(StringCalculator.Multiply, MultiplyNumbersMenuItemName);
+            //string selectedText = editor.GetSelText();
+            //decimal result = StringCalculator.Multiply(selectedText);
+            //MessageBox.Show(result.ToString(), MultiplyNumbersMenuItemName);
+        }
+
+        internal static void doOperation(Func<string, decimal> operation, string operationText)
+        {
+            string selectedText = editor.GetSelText();
+            if (string.IsNullOrEmpty(selectedText))
+            {
+                MessageBox.Show("No selection found.", operationText, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                decimal result = operation(selectedText);
+                MessageBox.Show(result.ToString(), operationText);
+            }
+            
+        }
+    }
+}
---
+++ src/SelTextCalculator/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelTextCalculator
+{
+    static class StringCalculator
+    {
+        static List<decimal> allNumbers = new List<decimal>();
+        private static void indentifyAllNumbers(string text)
+        {
+            
+            string[] tokens = text.Split(new char[] { ' ', '\r','\n' }, StringSplitOptions.RemoveEmptyEntries);
+            allNumbers.Clear();
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (decimal.TryParse(tokens[i], out decimal number))
+                {
+                    allNumbers.Add(number);
+                }
+            }
+            
+        }
+        public static decimal Sum(string text)
+        {
+            indentifyAllNumbers(text);
+            return allNumbers.Sum();            
+        }
+
+        public static decimal Multiply(string text)
+        {
+            indentifyAllNumbers(text);
+            return allNumbers.Aggregate( (a,d) => a *= d);
+        }
+    }
+}
